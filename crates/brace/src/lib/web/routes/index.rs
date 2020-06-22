a793c4b6590d85b93f84df1221c59e5567a3feba
Daniel Balcomb <daniel.balcomb@gmail.com>
2020-06-22 15:13:55 +0100

    Add web server configuration hook (#5)

--- crates/brace/src/lib/web/routes/index.rs
+++ crates/brace/src/lib/web/routes/index.rs
@@ -1,5 +1,13 @@
+use actix_web::web::{self, ServiceConfig};
 use actix_web::Responder;
 
-pub async fn get() -> impl Responder {
+use crate::util::hook;
+
+#[hook(crate::web::server::configure)]
+fn configure(serv: &mut ServiceConfig) {
+    serv.route("/", web::get().to(get));
+}
+
+async fn get() -> impl Responder {
     "brace"
 }
--- crates/brace/src/lib/web/server/mod.rs
+++ crates/brace/src/lib/web/server/mod.rs
@@ -1,9 +1,15 @@
 use std::net::Ipv4Addr;
 
-use actix_web::{web, App, HttpServer};
+use actix_web::web::ServiceConfig;
+use actix_web::{App, HttpServer};
+
+use crate::util::hook;
+
+#[hook]
+pub fn configure(serv: &mut ServiceConfig) {}
 
 pub async fn start(host: Ipv4Addr, port: u16) -> anyhow::Result<()> {
-    HttpServer::new(|| App::new().route("/", web::get().to(crate::web::routes::index::get)))
+    HttpServer::new(|| App::new().configure(|cfg| hook::exec(configure::with(cfg))))
         .bind((host, port))?
         .run()
         .await?;
